58591239c7a749842f99f174ea555e3154a6cbaf
Diego da Silva Pereira <diego.pereira@tema.group>
2020-07-11 02:42:23 -0300

    Board game module

---
+++ src/Board.hs
@@ -0,0 +1,46 @@
+module Board
+    ( Board(..)
+    , boardDealer
+    , boardSmallBlind
+    , boardBigBlind
+    , makeBoard
+    )
+where
+
+import           Deck
+import           Player
+
+data Board = Board {
+    common :: [Card],
+    remaining :: [Card],
+    players :: [Player]
+}
+
+instance Show Board where
+    show board = show $ players board
+
+getPlayerByRole :: Board -> Role -> Player
+getPlayerByRole board playerRole =
+    head $ filter (\player -> role player == playerRole) (players board)
+
+boardDealer :: Board -> Player
+boardDealer = flip getPlayerByRole Dealer
+
+boardSmallBlind :: Board -> Player
+boardSmallBlind = flip getPlayerByRole SmallBlind
+
+boardBigBlind :: Board -> Player
+boardBigBlind = flip getPlayerByRole BigBlind
+
+makeBoard :: Int -> Int -> Int -> Board
+makeBoard n cash seed = makeNewBoard deck cards (take n [Dealer ..]) players
+  where
+    (deck, cards) = drawCards (shuffleDeck makeFullDeck seed) 2
+    players       = []
+    makeNewBoard :: [Card] -> [Card] -> [Role] -> [Player] -> Board
+    makeNewBoard deck cards [] players =
+        Board { common = deck, remaining = cards, players = reverse players }
+    makeNewBoard deck cards (r : rs) players =
+        let (playerHand, tmpDeck) = drawCards deck 2
+            newPlayer = Player { hand = playerHand, role = r, cash = cash }
+        in  makeNewBoard tmpDeck cards rs (newPlayer : players)
